b79a37ceefc5c35d630eb511ca1e60aa8e5b06e0
RussianWeed <bwubta21166@brainwareuniversity.ac.in>
2024-03-26 17:26:41 +0530

    android

---
+++ app/src/main/java/com/devking/hotel_app_andoird/MainActivity.java
@@ -0,0 +1,14 @@
+package com.devking.hotel_app_andoird;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
